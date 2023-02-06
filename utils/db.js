cf2156dd0a9d06f556aecad30dc0aa25b77e851e
Luki Klekner <lukaszklekner@gmail.com>
2023-02-06 18:24:47 +0100

    feat: set up mongdodb

---
+++ utils/db.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+const dbName = 'todo';
+
+client.connect();
+
+const db = client.db(dbName);
+const todos = db.collection('todos')
+
+module.exports = {
+    db,
+    todos
+}
